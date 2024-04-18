5a58d60b4ff80a5a4c1cc2b17c4f7e2798f17e3f
Dru Sellers <dru@drusellers.com>
2024-04-18 13:02:59 -0500

    Rabbit MQ Client Playaround

---
+++ CoreRabbitMQ/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var cf = new ConnectionFactory();
+cf.UserName = "guest";
+cf.Password = "guest";
+cf.Port = 5672;
+using var conn = cf.CreateConnection("localhost");
+using var model = conn.CreateModel();
+
+// declare the queue
+model.QueueDeclare(queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var consumer = new EventingBasicConsumer(model);
+consumer.Received += (m, ea) =>
+{
+    var headers = ea.BasicProperties.Headers;
+
+    // var abc = headers["Content-Type"];
+    // if (abc.ToString() != "application/json")
+    // {
+    //     return;
+    // }
+    //
+    // var targetType = headers[""];
+    var body = ea.Body.ToArray();
+
+    // deserialize
+    // pipeline of my own
+
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+};
+
+model.BasicConsume(queue: "hello",
+    autoAck: true,
+    consumer: consumer);
+
+const string message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+model.BasicPublish(exchange: string.Empty,
+    routingKey: "hello",
+    basicProperties: null,
+    body: body);
+
+Console.WriteLine("...");
+Console.ReadLine();
---
+++ CoreRabbitMQ/README.md
@@ -0,0 +1,7 @@
+# Intercom Ideas
+
+```yaml
+Intercom:
+  Control: agent-123-control
+  Data: agent-123-data
+```
