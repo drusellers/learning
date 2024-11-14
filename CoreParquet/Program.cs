821502545d186f986112ed9aa652319352821752
Dru Sellers <dru@drusellers.com>
2024-11-13 20:46:47 -0600

    Parquet

---
+++ CoreParquet/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Parquet;
+
+var pwd = Environment.CurrentDirectory;
+var path = Path.Join(pwd, "AltasParquetDaily-00001.snappy.parquet");
+var p = await ParquetReader.ReadTableFromFileAsync(path);
+Console.WriteLine(p.Count);
